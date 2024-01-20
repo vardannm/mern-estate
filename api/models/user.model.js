addf8e44001b8f2d53a46c178d949835384d6747
vardannm <vardan.torosyan40@gmail.com>
2024-01-21 03:14:07 +0400

    create swign up api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,17 +1,23 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
- username:{
-    type:String,
-    required:true,
-    unique:true,
- },
- password:{
-    type:String,
-    required:true,
- }
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-},{timestamps:true});
-
-const User = new mongoose.model('User',userSchema);
+const User = new mongoose.model("User", userSchema);
 export default User;
